b412b8af259c162db85975c1e9c8158a44f54846
Vivek Sharma <vsharma@binarysemantics.com>
2021-03-10 07:21:03 +0530

    Third commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Verify Pipeline'));
+app.get('/', (req, res) => res.send('Verify Pipeline. Check for Auto pipeline exection.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
